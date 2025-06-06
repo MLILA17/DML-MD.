a039bf963139cbdaad4086c2ab12227cac5ef9fe
DML <daudymussa1705@gmail.com>
2025-06-06 23:31:33 +0300

    Update tool-tempmail.js

--- plugins/tool-tempmail.js
+++ plugins/tool-tempmail.js
@@ -55,7 +55,7 @@ _Email will expire after 24 hours_
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363363023106228@newsletter',
+                        newsletterJid: '120363387497418815@newsletter',
                         newsletterName: 'TempMail Service',
                         serverMessageId: 101
                     }
