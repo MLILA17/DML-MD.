a95f0a4bfd6c2328b91249916076ab21ced6bbf8
DML <daudymussa1705@gmail.com>
2025-06-06 23:56:46 +0300

    Update ytdl-fast.js

--- plugins/ytdl-fast.js
+++ plugins/ytdl-fast.js
@@ -35,7 +35,7 @@ cmd({
 👀 *Views:* ${yts.views}
 👤 *Author:* ${yts.author.name}
 🔗 *Link:* ${yts.url}
-> Powered By JawadTechX ❤️`;
+> Powered By Dml ❤️`;
 
         // Send video directly with caption
         await conn.sendMessage(
@@ -89,8 +89,8 @@ cmd({
             body: "Join our WhatsApp Channel",
             mediaType: 1,
             thumbnailUrl: song.thumbnail.replace('default.jpg', 'hqdefault.jpg'),
-            sourceUrl: 'https://whatsapp.com/channel/0029VatOy2EAzNc2WcShQw1j',
-            mediaUrl: 'https://whatsapp.com/channel/0029VatOy2EAzNc2WcShQw1j',
+            sourceUrl: 'https://whatsapp.com/channel/0029Vb2hoPpDZ4Lb3mSkVI3C',
+            mediaUrl: 'https://whatsapp.com/channel/0029Vb2hoPpDZ4Lb3mSkVI3C',
             showAdAttribution: true,
             renderLargerThumbnail: true
         }
