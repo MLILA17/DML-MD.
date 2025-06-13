a85a81ef56e7cc4eba5d3dd64e5c47ddaa972019
DML <daudymussa1705@gmail.com>
2025-06-13 16:32:51 +0300

    Update get-owner.js

--- plugins/get-owner.js
+++ plugins/get-owner.js
@@ -2,7 +2,7 @@ const { cmd } = require('../command');
 const config = require('../config');
 
 cmd({
-    pattern: "owner",
+    pattern: "owner1",
     react: "✅", 
     desc: "Get owner number",
     category: "main",
