2a034ac2ff98ccb81e55c9ebba93b8feb82b8407
DML <daudymussa1705@gmail.com>
2025-06-15 20:22:35 +0300

    Update movie.js

--- plugins/movie.js
+++ plugins/movie.js
@@ -54,7 +54,7 @@ async (conn, mek, m, { from, reply, sender, args }) => {
             from,
             {
                 image: { 
-                    url: movie.poster && movie.poster !== 'N/A' ? movie.poster : 'https://files.catbox.moe/7zfdcq.jpg'
+                    url: movie.poster && movie.poster !== 'N/A' ? movie.poster : 'https://files.catbox.moe/vcdwmp.jpg'
                 },
                 caption: dec,
                 contextInfo: {
